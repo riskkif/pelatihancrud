4086d9b5edf74106d88a7095fb748de08b95c52f
riskkif <riskisam78@gmail.com>
2024-07-04 09:46:57 +0700

    second commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+   
     protected $guarded = [];
-
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -36,6 +36,5 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
